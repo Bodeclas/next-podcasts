d6216c3ddc4c9c66c1cb4e14a11ca98b911ee489
Brayan Jesús Salcedo <brayanjsalcedo@gmail.com>
2018-07-19 17:22:43 -0400

    Agregado el header y el template de la lista de audios

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,71 @@
 
 
+import 'isomorphic-fetch'
+
 export default class extends React.Component {
+
+  static async getInitialProps() {
+    let req = await fetch('https://api.audioboom.com/channels/recommended')
+    let { body: channels } = await req.json()
+    return { channels }
+  }
+
   render() { 
-    return <h1>Component works!</h1>
+    const { channels } = this.props
+
+    return <div>
+      <header>Audio list</header>
+      <div className="channels">
+      { channels.map(channel => (
+        <div className="channel">
+          <img src={channel.urls.logo_image.original} />
+          <h2>{channel.title}</h2>
+        </div>
+      )) }
+      </div>
+
+
+      <style jsx>{`
+        header {
+          color: #fff;
+          background: #8756ca;
+          padding: 15px;
+          text-align: center;
+        }
+
+        .channels {
+          display: grid;
+          grid-gap: 15px;
+          padding: 15px;
+          grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+        }
+
+        .channel {
+          display: block;
+          border-radius: 3px;
+          box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+          margin-bottom: 0.5em;
+        }
+
+        .channel img {
+          width: 100%;
+        }
+
+        h2 {
+          padding: 5px;
+          font-size: 0.9em;
+          font-weight: 600;
+          margin: 0;
+          text-align: center;
+        }
+      `}</style>
+      <style jsx global>{`
+        body {
+          background: whitesmoke;
+          margin: 0;
+          font-family: system-ui;
+        }
+      `}</style>
+    </div>
   }
 }
